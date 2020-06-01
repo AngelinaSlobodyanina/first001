02d94aca04c50489895518c7e554f5c960545172
AngelinaSlobodyanina <slobodyaninaangelina@gmail.com>
2020-06-01 20:20:01 +0300

    commit1

---
+++ Lab003/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main()
+    {
+        String[] st = new String[2];
+        try{
+        System.out.println(st[5]);}
+        catch (Array)
+    }
+
+}
